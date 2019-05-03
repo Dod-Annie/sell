7c51ebe393000386b6f725be270c06c3bf1c15d4
Dod-Annie <936166900@qq.com>
2019-05-04 01:41:34 +0800

    add utils

--- src/main/java/com/imooc/controller/BuyerProductController.java
+++ src/main/java/com/imooc/controller/BuyerProductController.java
@@ -7,6 +7,8 @@ import com.imooc.dataobject.ProductCategory;
 import com.imooc.dataobject.ProductInfo;
 import com.imooc.service.CategoryService;
 import com.imooc.service.ProductService;
+import com.imooc.utils.ResultVOUtil;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -36,29 +38,33 @@ public class BuyerProductController {
     public ResultVO list(){
         //1.查询所有上架商品
         List<ProductInfo> productInfoList = productService.findUpAll();
-
         //2.查询类目（一次性查询）
-        List<Integer> categoryTypeList = new ArrayList<>();
+        List<Integer> categoryTypeList =
         productInfoList.stream()
                 .map(e -> e.getCategoryType())
                 .collect(Collectors.toList());
         List<ProductCategory> productCategoryList = categoryService.findByCategoryTypeIn(categoryTypeList);
 
         //3.数据拼装
+        List<ProductVO> productVOList = new ArrayList<>();
         for (ProductCategory productCategory : productCategoryList){
-            
+            ProductVO  productVO = new ProductVO();
+            productVO.setCategoryType(productCategory.getCategoryType());
+            productVO.setCategoryName(productCategory.getCategoryName());
+
+            List<ProductInfoVO> productInfoVOList = new ArrayList<>();
+            for (ProductInfo productInfo:productInfoList){
+                if(productInfo.getCategoryType().equals(productCategory.getCategoryType())){
+                    ProductInfoVO productInfoVO = new ProductInfoVO();
+                    BeanUtils.copyProperties(productInfo,productInfoVO);
+                    productInfoVOList.add(productInfoVO);
+                }
+            }
+            productVO.setProductInfoVOList(productInfoVOList);
+            productVOList.add(productVO);
         }
 
-        ResultVO resultVO = new ResultVO();
-        resultVO.setCode(0);
-        resultVO.setMsg("成功");
-
-        ProductVO productVO = new ProductVO();
-        ProductInfoVO productInfoVO = new ProductInfoVO();
-        productVO.setProductInfoVOList(Arrays.asList(productInfoVO));
-        resultVO.setData(Arrays.asList(productVO));
-
-        return resultVO;
+        return ResultVOUtil.success((productVOList));
 
     }
 }
